b22c223380220fa6425f528380d511b5b108e410
Max Tochilashvili <100846975+mTochilashvili@users.noreply.github.com>
2022-03-08 12:16:50 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
-// Copyright 2022 UNN-IASR
 #include "fun.h"
 
 int main() {
-    return 0;
+    const char* str = "Hello world! I am a programmer! ";
+    unsigned int result1 = faStr1(str);
+    unsigned int result2 = faStr2(str);
+    unsigned int result3 = faStr3(str);
+    cout << result1 << endl;
+    cout << result2 << endl;
+    cout << result3 << endl;
 }
