5a202d5a277150c6219f9099924612e0245954ca
Max Tochilashvili <100846975+mTochilashvili@users.noreply.github.com>
2022-03-08 14:51:45 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include "fun.h"
 
 int main() {
-    const char* str = "Hello world! I am a programmer!";
+    const char* str = "Hello world I am a programmer";
     unsigned int result1 = faStr1(str);
     unsigned int result2 = faStr2(str);
     unsigned int result3 = faStr3(str);
