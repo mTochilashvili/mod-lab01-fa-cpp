c89728d5e46f7b1dd7afc7a7cb1db532fb26a73c
Max Tochilashvili <100846975+mTochilashvili@users.noreply.github.com>
2022-03-08 12:17:15 +0300

    Update fun.h

--- include/fun.h
+++ include/fun.h
@@ -2,8 +2,12 @@
 #ifndef INCLUDE_FUN_H_
 #define INCLUDE_FUN_H_
 
-unsigned int faStr1(const char *str);
-unsigned int faStr2(const char *str);
-unsigned int faStr3(const char *str);
+unsigned int faStr1(const char* str);
+unsigned int faStr2(const char* str);
+unsigned int faStr3(const char* str);
+
+#include <iostream>
+
+using namespace std;
 
 #endif  // INCLUDE_FUN_H_
