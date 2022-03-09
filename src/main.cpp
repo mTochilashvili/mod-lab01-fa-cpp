24f66b3c7288810fc27ee9f1ee0ec4f313cd61ae
Max Tochilashvili <100846975+mTochilashvili@users.noreply.github.com>
2022-03-09 10:54:33 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,10 @@
 
 int main() {
     const char* str = "Hello world I am a programmer";
-    unsigned int result1 = faStr1(str);
-    unsigned int result2 = faStr2(str);
-    unsigned int result3 = faStr3(str);
-    cout << result1 << endl;
-    cout << result2 << endl;
-    cout << result3 << endl;
+    unsigned int str1 = faStr1(str);
+    unsigned int str2 = faStr2(str);
+    unsigned int str3 = faStr3(str);
+    std::cout << str1 << std::endl;
+    std::cout << str2 << std::endl;
+    std::cout << str3 << std::endl;
 }
